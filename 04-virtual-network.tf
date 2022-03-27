cec1e43d63c7e3715555917bae5a93b1e201d5ba
Ayo-Milua <ayomide@lbcit.ca>
2022-03-27 08:59:06 +0000

    pr-yaml

--- 04-virtual-network.tf
+++ 04-virtual-network.tf
@@ -10,7 +10,7 @@ locals {
 resource "azurerm_virtual_network" "myvnet1" {
   name                = "myfirstvirtualnetwork"
   location            = azurerm_resource_group.my_demo_rg2.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   resource_group_name = azurerm_resource_group.my_demo_rg2.name
 
   tags = {
@@ -30,20 +30,20 @@ resource "azurerm_subnet" "public_subnet" {
 
 # Resource-4 Create Public IP Address
 
-# resource "azurerm_public_ip" "public_ip_01" {
-#   name                = "public_ip_address_01"
-#   resource_group_name = azurerm_resource_group.my_demo_rg2.name
-#   location            = azurerm_resource_group.my_demo_rg2.location
-#   allocation_method   = "Static"
-#   ip_version = "IPv4"
-#   sku = "Standard"
-#   availability_zone = "Zone-Redundant"
-#   domain_name_label = "myterraformlabel${random_uuid.dns.result}"
-#   tags = {
-#     environment = local.environment
-#     owner = local.owner
-#   }
-# }
+resource "azurerm_public_ip" "public_ip_01" {
+  name                = "public_ip_address_01"
+  resource_group_name = azurerm_resource_group.my_demo_rg2.name
+  location            = azurerm_resource_group.my_demo_rg2.location
+  allocation_method   = "Static"
+  ip_version = "IPv4"
+  sku = "Standard"
+  availability_zone = "Zone-Redundant"
+  domain_name_label = "myterraformlabel${random_uuid.dns.result}"
+  tags = {
+    environment = local.environment
+    owner = local.owner
+  }
+}
 
 
 # Resource-5 Create Newtork-Interface
